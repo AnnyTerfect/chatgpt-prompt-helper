fe9f195998747f595567a0ada220df0a30200db2
AnnyTerfect <1247470246@qq.com>
2023-05-28 11:25:56 +0800

    refactor: function to component

--- src/components/Buttons/TypeButton/index.jsx
+++ src/components/Buttons/TypeButton/index.jsx
@@ -3,40 +3,38 @@ import PropTypes from 'prop-types';
 import BaseButton from '../BaseButton';
 import styles from './style.module.scss';
 
-function getTypeButton(type) {
-  if (type === 'edit') {
-    return (
-      <path
-        className={styles.button}
-        d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"
-      />
-    );
+function ButtonContent({ type }) {
+  switch (type) {
+    case 'edit':
+      return (
+        <path
+          className={styles.button}
+          d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"
+        />
+      );
+    case 'finish':
+      return <polyline className={styles.button} points="20 6 9 17 4 12" />;
+    case 'delete':
+      return (
+        <g className={styles.button}>
+          <polyline points="3 6 5 6 21 6" />
+          <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
+          <line x1="10" y1="11" x2="10" y2="17" />
+          <line x1="14" y1="11" x2="14" y2="17" />
+        </g>
+      );
+    default:
+      throw new Error('Unknown button type');
   }
-  if (type === 'finish') {
-    return (
-      <polyline
-        className={styles.button}
-        points="20 6 9 17 4 12"
-      />
-    );
-  }
-  if (type === 'delete') {
-    return (
-      <g className={styles.button}>
-        <polyline points="3 6 5 6 21 6" />
-        <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
-        <line x1="10" y1="11" x2="10" y2="17" />
-        <line x1="14" y1="11" x2="14" y2="17" />
-      </g>
-    );
-  }
-  throw new Error('Unknown button type');
 }
+ButtonContent.propTypes = {
+  type: PropTypes.oneOf(['edit', 'finish', 'delete']).isRequired,
+};
 
 function TypeButton({ className, onClick, type }) {
   return (
     <BaseButton className={className} onClick={onClick}>
-      {getTypeButton(type)}
+      <ButtonContent type={type} />
     </BaseButton>
   );
 }
